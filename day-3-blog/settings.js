f326f892ca8e1c1b6ad541a6aaa2f338debf1773
AR Dahal <dahalaishraj@gmail.com>
2013-02-11 19:06:02 +0530

    Adding support for db and async

--- day-3-blog/settings.js
+++ day-3-blog/settings.js
@@ -2,9 +2,9 @@ modules.export = {
     db: {
         host: '127.0.0.1' ,
         port: 27017,
-        name: 'justjsblogdemo'
+        name: 'nodeingblogdemo'
     },
     http: {
-        port: 3000
+        port: 8080 
     }
 };
